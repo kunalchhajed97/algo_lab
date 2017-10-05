c0130e4c8a3b515ec8365d4c66f402957a34e275
utkarshagarwal01 <utkarsh.agarwal@learner.manipal.edu>
2017-10-05 11:17:23 +0530

    Update closedhash.c

--- week9/closedhash.c
+++ week9/closedhash.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define tablesize 30 
+#define tablesize 13
 typedef struct node{
 	char* string;
-	struct node* next;
 }Node;
-Node* newTable(int n)
-{
-	Node* a=(Node*)malloc(sizeof(Node));int i;
-	Node* head = a;
-	for(i=0;i<n-1;i++)
-	{
-		a->string = NULL;
-		a->next = (Node*)malloc(sizeof(Node));
-		a=a->next;
-	}
-	a->next = head;
-	return head;
-}
 int hashcode(char* string)
 {
 	int i,count = 0;
 	for(i=0;i<strlen(string);i++)
-		count+=string[i]-65;
+		count+=string[i]-64;
 	return count%tablesize;
 }
-int insert(Node* head,char* str)
+int insert(Node* head,int pos,char* str)
 {
-	while(head->string!=NULL)
-		head = head->next;
-	head->string = (char*)malloc((strlen(str)+1)*sizeof(char));;
-	strcpy(head->string,str);
+	while(head[pos].string!=NULL)
+		pos = (pos+1)%tablesize;
+
+	head[pos].string = (char*)malloc((strlen(str)+1)*sizeof(char));;
+	strcpy(head[pos].string,str);
 }
-int search(Node* head,char* buffer)
+int search(Node head[],int pos,char* buffer)
 {
-	while(head!=NULL || strcmp(head->string,buffer)==0)
-		head=head->next;
-	if(head==NULL) return 0; else return 1;
+	while(head[pos].string!=NULL)
+	{
+		if(strcmp(head[pos].string,buffer)==0)
+			return 1;
+		pos=(pos+1)%tablesize;
+	}
+	return 0;	
 }
 int main()
 {
@@ -45,14 +36,22 @@ int main()
 	printf("Enter no. of words to be inserted: ");
 	scanf("%d",&n);
 	char buffer[100];
-	Node* table = newTable(30);
+	Node* table = (Node*)calloc(tablesize,sizeof(Node));
 	for(i=0;i<n;i++)
 	{
 		memset(buffer,0,100);
 		scanf("%s",buffer);
 		int code = hashcode(buffer);
-		insert(table+code,buffer);
+		insert(table,code,buffer);
 	}
+	for(i=0;i<tablesize;i++)
+	{
+		if(table[i].string!=NULL)
+			printf("%s\n",table[i].string);
+		else
+			printf("NULL\n");
+	}
+
 	printf("Enter number of words to be searched: ");
 	scanf("%d",&n);
 	for(i=0;i<n;i++)
@@ -60,7 +59,7 @@ int main()
 		memset(buffer,0,100);
 		scanf("%s",buffer);
 		int code = hashcode(buffer);
-		if(search(table+code,buffer))
+		if(search(table,code,buffer))
 			printf("Found\n");
 		else
 			printf("Not Found\n");
