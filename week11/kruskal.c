7a756b6579ac147712a04c8b426cd3678f0dca6c
utkarshagarwal01 <coolguy.agarwal365@gmail.com>
2017-10-20 02:06:09 +0530

    Competed kruskals without using union find

--- week11/kruskal.c
+++ week11/kruskal.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-int dfs(int** arr, int visited[],int i,int n)
+int inArray(int arr[],int len,int ele)
+{
+	int i;
+	for(i=0;i<len;i++)
+		if(ele == arr[i])
+			return 1;
+	return 0;
+}
+int dfs(int** arr, int visited[],int i,int n,int stack[],int* index)
 {
 	int j;
 	visited[i]=1;
+	stack[(*index)++] = i;
 	for(j=0;j<n;j++)
 	{
-		if(arr[i][j] && visited[j] && j<i)
+		if(arr[i][j] && visited[j] && inArray(stack,(*index)-2,j))
 			return 0;
 		if(arr[i][j] && !visited[j])
-			dfs(arr,visited,j,n);
+			if (!dfs(arr,visited,j,n,stack,index))
+				return 0;
 	}
+	(*index)--;
 	return 1;
 }
 int cyclic(int** arr,int n)
 {
-	int i,j;int* visited = (int*)calloc(n,sizeof(int));
-	printf("\n\n");
-	for(i=0;i<n;i++)
-	{
-		for(j=0;j<n;j++)
-			printf("%d ", arr[i][j]);
-		printf("\n");
-	}
+	int i,j,index=0;
+	int* visited = (int*)calloc(n,sizeof(int));
+	int* stack = (int*)calloc(n,sizeof(int));
 	for (i = 0; i < n; i++)
-	{
 		if(!visited[i])
-			if(!dfs(arr,visited,i,n))
-			{
-				printf("cyclic\n");
+			if(!dfs(arr,visited,i,n,stack,&index))
 				return 1;
-			}
-			else
-				printf("Non cyclic\n");
-	}
 	return 0;
 }
 int** kruskals(int n,int** edges,int x)
@@ -42,13 +41,13 @@ int** kruskals(int n,int** edges,int x)
 	int** arr = (int**)calloc(n,sizeof(int*));
 	for(i=0;i<n;i++)
 		arr[i] = (int*)calloc(n,sizeof(int));
-	while(k<n-1)
+	while(ecounter<n-1 && k<x)
 	{
-		arr[edges[k][0]][edges[k][1]]=edges[k][2];
+		arr[edges[k][0]][edges[k][1]]=arr[edges[k][1]][edges[k][0]]=edges[k][2];
 		if(!cyclic(arr,n))
 			ecounter++;
 		else
-			arr[edges[k][0]][edges[k][1]]=0;
+			arr[edges[k][0]][edges[k][1]]=arr[edges[k][1]][edges[k][0]]=0;
 		k++;
 	}
 	return arr;
@@ -58,6 +57,7 @@ int main()
 	printf("Enter number of nodes\n");
 	int n,i,j,k,x=0;scanf("%d",&n);
 	int** edges = (int**)calloc(n*(n-1),sizeof(int*));
+	printf("Enter distance matrix\n");
 	for(i=0;i<n;i++)
 		for(j=0;j<n;j++)
 		{
@@ -85,7 +85,7 @@ int main()
 		}
 	}
 	int** arr = kruskals(n,edges,x);
-	printf("output\n");
+	printf("Output\n");
 	for(i=0;i<n;i++)
 	{
 		for(j=0;j<n;j++)
